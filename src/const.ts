3f6781648436c504b1313cc47a82e2666d185897
B3rzxd3v <d3vilc0re@gmail.com>
2023-04-29 13:29:11 +0700

    First commit

---
+++ src/const.ts
@@ -0,0 +1,22 @@
+export const headerMenu = [
+  {
+    classPostfix: 'goblin',
+    description: 'Goblin Online',
+    src: 'https://goblins-online.ru/'
+  },
+  {
+    classPostfix: 'zagonka',
+    description: 'Zagonka',
+    src: 'http://zagonka1.zagonkov.gb.net/9'
+  },
+  {
+    classPostfix: 'vk',
+    description: 'Vkontakte',
+    src: 'https://vk.com/'
+  },
+  {
+    classPostfix: 'igromania',
+    description: 'Igromania',
+    src: 'https://www.igromania.ru/'
+  }
+];
